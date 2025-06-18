4edb844245e2c21dddd3b986e19c2d8f49bd88a7
Bianca Salomao <biancasalomao2024@gmail.com>
2025-06-18 10:51:57 -0300

    starting project structure

---
+++ src/test/java/com/projetoPessoal/ProjetoPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoPessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
